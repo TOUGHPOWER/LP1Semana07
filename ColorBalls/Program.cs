8e4dc7e6d1140164347dce9c59ecb887d8b9c5e7
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-04-28 13:21:39 +0100

    Test in program of throwing and popping balls plus printing results

--- ColorBalls/Program.cs
+++ ColorBalls/Program.cs
@@ -6,7 +6,29 @@ namespace ColorBalls
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Color color1 = new Color(50,100,200);
+            Color color2 = new Color(50,55,100,150);
+            Ball ball1 = new Ball(color1,15);
+            Ball ball2 = new Ball(color2,5);
+            ball1.Throw();
+            ball1.Pop();
+            ball1.Throw();
+            ball2.Throw();
+            ball2.Throw();
+            ball2.Pop();
+
+            Console.Write($"Ball 1 has {color1.GetRedValue()} of red |");
+            Console.Write($"Ball 1 has {color1.GetGreenValue()} of green |");
+            Console.WriteLine($"Ball 1 has {color1.GetBlueValue()} of blue |");
+            Console.WriteLine($"Ball 1 has {color1.GetAlphaValue()} of transparency");
+            Console.WriteLine($"Ball 1 has been thrown {ball1.GetNumberOfTimesThrown()} times");
+            Console.Write($"Ball 1 has {color2.GetRedValue()} of red |");
+            Console.Write($"Ball 1 has {color2.GetGreenValue()} of green |");
+            Console.Write($"Ball 1 has {color2.GetBlueValue()} of blue |");
+            Console.WriteLine($"Ball 1 has {color2.GetAlphaValue()} of transparency");
+            Console.WriteLine($"Ball 1 has been thrown {ball2.GetNumberOfTimesThrown()} times");
+
+
         }
     }
 }
